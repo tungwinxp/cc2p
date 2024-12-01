f48a61a8b399b97b7c35dbbe2b01e7f534b7d34a
Tung H. Nguyen <111020529+tungwinxp@users.noreply.github.com>
2024-12-01 21:56:59 +0000

    fix target output

--- src/lib.rs
+++ src/lib.rs
@@ -61,8 +61,11 @@ pub fn convert_to_parquet(
         .build(file)?;
 
     // Determine the target file path
-    let target_file = output_file.unwrap_or_else(|| &file_path.with_extension("parquet"));
-
+    let target_file = if let Some(output) = output_file {
+        output
+    } else {
+        &file_path.with_extension("parquet")
+    };
 
     // Delete the output file if it exists
     delete_if_exist(target_file.to_str().unwrap())?;
